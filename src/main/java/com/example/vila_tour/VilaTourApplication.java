b971f725ac64fc5a637854d0013ac275d7975cd0
Aitor Agullo Duque <dam_ada@informatica.mz>
2024-10-15 11:33:24 +0200

    0

---
+++ src/main/java/com/example/vila_tour/VilaTourApplication.java
@@ -0,0 +1,13 @@
+package com.example.vila_tour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VilaTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VilaTourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vila_tour/VilaTourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vila_tour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VilaTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
