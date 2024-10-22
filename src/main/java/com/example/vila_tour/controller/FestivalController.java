ec21202ef3a984a9e812fb92b7593ff33f572da8
Aitor <aduqueagullo@gmail.com>
2024-10-22 12:55:56 +0200

    Revisada y Modificada clase Festivals

--- src/main/java/com/example/vila_tour/controller/FestivalController.java
+++ src/main/java/com/example/vila_tour/controller/FestivalController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Set;
 
 @RequestMapping("/festivals")
@@ -20,16 +22,69 @@ public class FestivalController {
     @GetMapping(value = "")
     public ResponseEntity<Set<Festival>> getFestivals() {
         Set<Festival> festivals;
-        festivals = festivalService.findAll();
+        festivals = festivalService.findAllFestivals();
         return new ResponseEntity<>(festivals, HttpStatus.OK);
     }
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Festival> getFestival(@PathVariable long id){
-        Festival festival = festivalService.findById(id)
+        Festival festival = festivalService.findFestivalById(id)
                 .orElseThrow(() -> new FestivalNotFoundException(id));
 
         return new ResponseEntity<>(festival, HttpStatus.OK);
+    }
+
+    @GetMapping("/search/description")
+    public ResponseEntity<Set<Festival>> findFestivalsByDescription(@RequestParam String description) {
+        Set<Festival> festivals = festivalService.findFestivalsByDescription(description);
+        return new ResponseEntity<>(festivals, HttpStatus.OK);
+    }
+
+    @GetMapping("/search/score")
+    public ResponseEntity<Set<Festival>> findFestivalsByAverageScore(@RequestParam double averageScore) {
+        Set<Festival> festivals = festivalService.findFestivalsByAverageScore(averageScore);
+        return new ResponseEntity<>(festivals, HttpStatus.OK);
+    }
+
+    @GetMapping("/search/startDate")
+    public ResponseEntity<Set<Festival>> findFestivalsByStartDate(@RequestParam String startDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate parsedStartDate = LocalDate.parse(startDate, formatter);
+        Set<Festival> festivals = festivalService.findFestivalsByStartDate(parsedStartDate);
+        return new ResponseEntity<>(festivals, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/sorted")
+    public ResponseEntity<Set<Festival>> getFestivalsSortedByName() {
+        Set<Festival> festivals;
+        festivals = festivalService.findAllByOrderByName();
+        return new ResponseEntity<>(festivals, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/sortedInverse")
+    public ResponseEntity<Set<Festival>> getFestivalsSortedByNameInverse() {
+        Set<Festival> festivals;
+        festivals = festivalService.findAllByOrderByNameDesc();
+        return new ResponseEntity<>(festivals, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "")
+    public ResponseEntity<Set<Festival>> getFestivalsByNameAndAverageScore(
+            @RequestParam(value = "name", defaultValue = "") String name,
+            @RequestParam(value = "averageScore", defaultValue = "-1") double averageScore) {
+
+        Set<Festival> festivals;
+        festivals = festivalService.findByNameAndAverageScore(name, averageScore);
+        return new ResponseEntity<>(festivals, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "")
+    public ResponseEntity<Set<Festival>> getFestivalsByNameContaining(
+            @RequestParam(value = "name", defaultValue = "") String name) {
+
+        Set<Festival> festivals;
+        festivals = festivalService.findByNameContaining(name);
+        return new ResponseEntity<>(festivals, HttpStatus.OK);
     }
 
     @PostMapping("")
@@ -39,9 +94,8 @@ public class FestivalController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Festival> modifyFestival(@PathVariable long id,
-                                           @RequestBody Festival festival){
-        Festival newFestival = festivalService.modifyFestival(id, festival);
+    public ResponseEntity<Festival> modifyFestival(@PathVariable long id, @RequestBody Festival newFestival){
+        Festival festival = festivalService.modifyFestival(id, newFestival);
         return new ResponseEntity<>(newFestival,HttpStatus.OK);
     }
 
