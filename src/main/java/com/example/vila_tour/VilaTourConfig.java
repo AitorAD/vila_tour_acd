b2c5b2eda4bddd954a905c094a4961b49fb885a3
Omar Llinares <llinares.omar@gmail.com>
2024-10-22 12:35:50 +0200

    Clase Config de Swagger, y documentado Article y ArticleController

---
+++ src/main/java/com/example/vila_tour/VilaTourConfig.java
@@ -0,0 +1,24 @@
+package com.example.vila_tour;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class VilaTourConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("VilaTour API")
+                        .description("VilaTour API REST")
+                        .contact(new Contact()
+                                .name("Team AJO")
+                                .email("contacto@vilaTour.es")
+                                .url("www.vilatour.es"))
+                        .version("1.0"));
+    }
+}
