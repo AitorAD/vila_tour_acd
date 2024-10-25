72408b2025873aba0187e78b204b6bc970759525
Omar Llinares <llinares.omar@gmail.com>
2024-10-25 13:56:27 +0200

    favorito
    wqdas

--- src/main/java/com/example/vila_tour/domain/Review.java
+++ src/main/java/com/example/vila_tour/domain/Review.java
@@ -1,5 +1,6 @@
 package com.example.vila_tour.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 @Entity
 @Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "article_id"})})
@@ -18,7 +19,15 @@ public class Review {
     @JoinColumn(name = "article_id", nullable = false)
     private Article article;
 
+    @Schema(description = "Puntuacion del usuario al articulo", example = "4.5")
+    @Column
     private long rating;
+
+    @Schema(description = "Comentario de la review", example = "Me gustó mucho, Javi RR un crack")
+    @Column
     private String comment;
 
+    @Schema(description = "Marca como favorito", example = "false")
+    @Column
+    private boolean favorite;
 }
