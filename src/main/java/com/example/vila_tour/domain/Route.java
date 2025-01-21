50b70a3ba2c9dd857137963a80363a8bf9cc351e
Javier Otero <javii.oteero@gmail.com>
2025-01-21 13:39:37 +0100

    permisos documentacion

--- src/main/java/com/example/vila_tour/domain/Route.java
+++ src/main/java/com/example/vila_tour/domain/Route.java
@@ -1,5 +1,6 @@
 package com.example.vila_tour.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
