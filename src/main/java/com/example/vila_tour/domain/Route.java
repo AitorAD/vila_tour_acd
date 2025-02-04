a27427b594ae09fe80a7f6c220dce9f5775f9d2d
Omar Llinares <llinares.omar@gmail.com>
2025-02-04 12:58:02 +0100

    Añadidia descripciona a la ruta

--- src/main/java/com/example/vila_tour/domain/Route.java
+++ src/main/java/com/example/vila_tour/domain/Route.java
@@ -1,6 +1,5 @@
 package com.example.vila_tour.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,6 +19,9 @@ public class Route {
 
     @Column(name = "name")
     private String name;
+
+    @Column(name = "description")
+    private String description;
 
     @ManyToMany(cascade = CascadeType.DETACH)
     @JoinTable(name = "route_place",
